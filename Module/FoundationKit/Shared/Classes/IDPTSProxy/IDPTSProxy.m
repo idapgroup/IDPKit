f38606bac00635297d4b39c6cd2f7265d2bcb055
Oleksa 'trimm' Korin <oleksandr.korin@idapgroup.com>
2015-02-06 16:26:58 +0200

    IDPTSProxy -> initial commit

---
+++ Module/FoundationKit/Shared/Classes/IDPTSProxy/IDPTSProxy.m
@@ -0,0 +1,13 @@
+//
+//  IDPTSProxy.m
+//  iOS
+//
+//  Created by Oleksa Korin on 6/2/15.
+//  Copyright (c) 2015 IDAP Group. All rights reserved.
+//
+
+#import "IDPTSProxy.h"
+
+@implementation IDPTSProxy
+
+@end
