02cc98db9db1277b6451e458f95d82296b41f702
Alexander Kradenkov <alexander.kradenkov@idapgroup.com>
2015-04-30 16:36:50 +0300

    IDPBackgroundOperationSpec.m -> IDPBackgroundOperation test

--- Tests/iOS/iOSTests/Specs/FoundationKit/Classes/IDPBackgroundOperation/IDPBackgroundOperationSpec.m
+++ Tests/iOS/iOSTests/Specs/FoundationKit/Classes/IDPBackgroundOperation/IDPBackgroundOperationSpec.m
@@ -13,16 +13,72 @@
 SPEC_BEGIN(IDPBackgroundOperationSpec)
 
 describe(@"IDPBackgroundOperation", ^{
-    context(@"after operation object created", ^{
-        IDPBackgroundOperation *operation = [IDPBackgroundOperation new];
-        it(@"should be not nil", ^{
+    context(@"after operation being created", ^{
+        __block IDPBackgroundOperation *operation = nil;
+        beforeAll(^{
+            operation = [IDPBackgroundOperation new];
+        });
+        
+        it(@"shouldn't be nil", ^{
             [[operation shouldNot] beNil];
         });
         
-        context(@"after empty operation being started", ^{
-            [operation start];
-            it(@"shoul finish immediately", ^{
-                [[theValue(operation.isFinished) should] equal:@(YES)];
+        it(@"shoul be asynchronous", ^{
+            [[theValue(operation.isAsynchronous) should] beYes];
+        });
+        
+        it(@"shouldn't be finished", ^{
+            [[theValue(operation.isFinished) shouldNot] beYes];
+        });
+
+        it(@"shouldn't be executed", ^{
+            [[theValue(operation.isExecuting) shouldNot] beYes];
+        });
+        
+        it(@"should be kind of NSOperation class", ^{
+            [[operation should] beKindOfClass:[NSOperation class]];
+        });
+        
+        context(@"while operation being started", ^{
+            it(@"should eventually receive @selector(main)", ^{
+                [[operation shouldNot] receive:@selector(main)];
+                [[operation shouldEventually] receive:@selector(main)];
+                [operation start];
+            });
+        });
+    });
+    
+    context(@"after operation being created", ^{
+        __block IDPBackgroundOperation *operation = nil;
+        beforeAll(^{
+            operation = [IDPBackgroundOperation new];
+        });
+        
+        context(@"while operation being started", ^{
+            it(@"should eventually receive @selector(complete)", ^{
+                [[operation shouldNot] receive:@selector(complete)];
+                [[operation shouldEventually] receive:@selector(complete)];
+                [operation start];
+            });
+        });
+    });
+    
+    context(@"after another operation being created", ^{
+        __block IDPBackgroundOperation *operation = nil;
+        beforeAll(^{
+            operation = [IDPBackgroundOperation new];
+        });
+        
+        context(@"while operation being added to queue", ^{
+            __block NSOperationQueue *queue = nil;
+            
+            beforeAll(^{
+                queue = [NSOperationQueue new];
+            });
+            
+            it(@"shouldn't eventually receive @selector(complete)", ^{
+                [[operation shouldEventually] receive:@selector(complete)];
+                [queue addOperation:operation];
             });
         });
     });
