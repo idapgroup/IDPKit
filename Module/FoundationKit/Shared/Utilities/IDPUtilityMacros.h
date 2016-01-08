456b34a0343aaff38381876f6b6dbab342a8b06f
Artem Chabannyi <artem.chabanniy@gmail.com>
2016-01-08 12:45:02 +0200

    Added macros which generate property for main view.

--- Module/FoundationKit/Shared/Utilities/IDPUtilityMacros.h
+++ Module/FoundationKit/Shared/Utilities/IDPUtilityMacros.h
@@ -16,10 +16,21 @@
 #define IDPNonatomicRetainPropertySynthesizeWithObserver(ivar, newObj) do{if(ivar!=newObj){[ivar removeObserver:self];[ivar release];ivar=[newObj retain];[ivar addObserver:self];}}while(0)
 
 #define IDPViewControllerViewOfClassGetterSynthesize(theClass, getterName) \
-            - (theClass *)getterName { \
-                            if ([self.view isKindOfClass:[theClass class]]) { \
-                                return (theClass *)self.view; \
-                            } \
-                            return nil; \
-                        }
+    - (theClass *)getterName { \
+        if ([self.view isKindOfClass:[theClass class]]) { \
+            return (theClass *)self.view; \
+        } \
+            return nil; \
+    }
 
+#define IDPViewControllerViewPropertyDefinition(theViewClass, propertyName) \
+    @property (nonatomic, strong, readonly) theViewClass   *propertyName;
+
+#define IDPViewControllerViewProperty(viewControllerClass, theViewClass, propertyName) \
+    @interface viewControllerClass (__##theViewCall##_##propertyName) \
+        IDPViewControllerViewPropertyDefinition(theViewClass, propertyName) \
+    @end \
+    \
+    @implementation viewControllerClass (__##theViewCall##_##propertyName) \
+        IDPViewControllerViewOfClassGetterSynthesize(theViewClass, propertyName) \
+    @end
