cd6902b8185f224e135641899b3d26ecad1dd3cc
Oleksa 'trimm' Korin <oleksandr.korin@idapgroup.com>
2016-01-12 18:27:02 +0200

    Initial test specs
    NSNull+IDPNilSpec -> initial test commit

--- Tests/iOS/iOSTests/Specs/FoundationKit/Categories/NSNull/NSNull+IDPNilSpec.m
+++ Tests/iOS/iOSTests/Specs/FoundationKit/Categories/NSNull/NSNull+IDPNilSpec.m
@@ -8,70 +8,86 @@
 
 #import <Kiwi.h>
 
+#import "IDPObjCRuntime.h"
+
 #import "NSNull+IDPNil.h"
+
+typedef void(^IDPSELBlock)(SEL selector);
 
 SPEC_BEGIN(NSNull_IDPNilSpec)
 
 describe(@"NSNull+IDPNil", ^{
+    NSNull *nullValue = [NSNull new];
+    id value = nullValue;
+    
+    IDPSELBlock shouldNotRaiseBlock = ^(SEL selector) {
+        it(@"shouldn't raise", ^{
+#pragma clang diagnostic push
+#pragma clang diagnostic ignored "-Warc-performSelector-leaks"
+            [[[nullValue performSelector:selector] shouldNot] raise];
+#pragma clang diagnostic pop
+        });
+    };
+    
     context(@"when sending unknown message ", ^{
         context(@"with void return type", ^{
-            it(@"shouldn't throw", ^{
-                
-            });
+            shouldNotRaiseBlock(@selector(loadView));
         });
         
         context(@"with object return type", ^{
-            it(@"shouldn't throw", ^{
-                
-            });
+            shouldNotRaiseBlock(@selector(view));
             
             it(@"should return nil", ^{
-                
+                [[theValue([value view]) should] beNil];
             });
         });
         
         context(@"with primitive value return type", ^{
-            it(@"shouldn't throw", ^{
-                
-            });
+            shouldNotRaiseBlock(@selector(count));
             
             it(@"should return 0", ^{
-                
+                [[theValue([value count]) should] beZero];
             });
         });
         
         context(@"with struct value return type", ^{
-            it(@"shouldn't throw", ^{
-                
-            });
+            shouldNotRaiseBlock(@selector(frame));
             
             it(@"should return 0", ^{
-                
+                CGRect frame = [value frame];
+                [[theValue(CGRectIsNull(frame)) should] beYes];
             });
         });
     });
     
     context(@"when sending NSNull methods", ^{
-        it(@"shouldn't throw", ^{
-            
-        });
+        shouldNotRaiseBlock(@selector(description));
+        shouldNotRaiseBlock(@selector(class));
     });
     
     context(@"when comparing", ^{
+        it(@"shouldn't raise", ^{
+            [[theBlock(^{ [nullValue isEqual:[NSNull null]]; }) shouldNot] raise];
+        });
+        
         it(@"should return YES, when comparing with nil", ^{
-            
+            [[theValue([nullValue isEqual:nil]) should] beYes];
         });
         
         it(@"should return YES, when comparing with NSNull", ^{
-            
+            [[theValue([nullValue isEqual:[NSNull null]]) should] beYes];
+        });
+        
+        it(@"should return YES, when comparing with self", ^{
+            [[theValue([nullValue isEqual:nullValue]) should] beYes];
         });
         
         it(@"should return NO, when comparing with any other object", ^{
-            
+            [[theValue([nullValue isEqual:[NSObject new]]) should] beNo];
         });
         
         it(@"should return same hash for two NSNull objects", ^{
-            
+            [[theValue([nullValue hash]) should] equal:theValue([[NSNull null] hash])];
         });
     });
 
