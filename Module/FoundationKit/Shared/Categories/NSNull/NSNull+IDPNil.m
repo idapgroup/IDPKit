3d1095212baafb9de6b48a19584c46342a967808
Oleksa 'trimm' Korin <oleksandr.korin@idapgroup.com>
2016-01-12 21:43:52 +0200

    NSNull+IDPNil -> added methodSingatureForSelector programmatic interception

--- Module/FoundationKit/Shared/Categories/NSNull/NSNull+IDPNil.m
+++ Module/FoundationKit/Shared/Categories/NSNull/NSNull+IDPNil.m
@@ -8,6 +8,57 @@
 
 #import "NSNull+IDPNil.h"
 
+#import <objc/runtime.h>
+
+typedef NSMethodSignature *(*IDPMethodSignatureForSelectorIMP)(id, SEL, SEL);
+typedef id(^IDPBlockWithIMPBlock)(IMP implementation);
+
 @implementation NSNull (IDPNil)
+
++ (void)load {
+//    IMP forwardInvocationIMP = [self instanceMethodForSelector:@selector(forwardInvocation:)];
+    [self replaceMethodSignatureForSelector];
+    [self replaceForwardInvocation];
+}
+
+// TODO: explain what it does
++ (void)replaceMethodSignatureForSelector {
+    SEL methodSignatureForSelectorSEL = @selector(methodSignatureForSelector:);
+    
+    IDPBlockWithIMPBlock block = ^(IMP implementation){
+        IDPMethodSignatureForSelectorIMP methodSignatureForSelectorIMP = (IDPMethodSignatureForSelectorIMP)implementation;
+        
+        return (id)^(NSNull *null, SEL selector) {
+            id methodSingature = methodSignatureForSelectorIMP(null, methodSignatureForSelectorSEL, selector);
+            if (methodSingature) {
+                return methodSingature;
+            }
+            
+            return (id)methodSignatureForSelectorIMP(null, methodSignatureForSelectorSEL, @selector(fakeMethod));
+        };
+    };
+    
+    [self setBlock:block forSelector:@selector(methodSignatureForSelector:)];
+}
+
++ (void)replaceForwardInvocation {
+    
+}
+
++ (void)setBlock:(IDPBlockWithIMPBlock)block forSelector:(SEL)selector {
+    IMP implementation = [self instanceMethodForSelector:selector];;
+    
+    IMP blockIMP = imp_implementationWithBlock(block(implementation));
+    
+    Method method = class_getInstanceMethod(self, selector);
+    class_replaceMethod(self,
+                        selector,
+                        blockIMP,
+                        method_getTypeEncoding(method));
+}
+
+- (void)fakeMethod {
+
+}
 
 @end
