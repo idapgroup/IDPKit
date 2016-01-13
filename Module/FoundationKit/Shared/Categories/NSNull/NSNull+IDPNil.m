f22dc0feba26f06657172719bdc79dc5f3f62958
Oleksa 'trimm' Korin <oleksandr.korin@idapgroup.com>
2016-01-13 18:35:49 +0200

    NSNull+IDPNil -> implemented replaceForwardInvocation, refactored the code

--- Module/FoundationKit/Shared/Categories/NSNull/NSNull+IDPNil.m
+++ Module/FoundationKit/Shared/Categories/NSNull/NSNull+IDPNil.m
@@ -9,43 +9,79 @@
 #import "NSNull+IDPNil.h"
 
 #import "NSObject+IDPRuntime.h"
+#import "NSMethodSignature+IDPNilPrivate.h"
 
 typedef NSMethodSignature *(*IDPMethodSignatureForSelectorIMP)(id, SEL, SEL);
+typedef void(*IDPForwardInvocationIMP)(id, SEL, id);
+
+@interface NSNull (IDPNilPrivate)
+
+// Replaces methodSignatureForSelector: imp with imp, that calls original implementation and returns its result,
+// if it's non-nil. Otherwise it returns fakeMethod method signature.
++ (void)replaceMethodSignatureForSelector;
+
+// Replaces forwardInvocation: imp with imp, that calls original implementation and returns its result,
+// if NSInvocation methodSignature is unrelated to fakeMetohd. Otherwise it executes NSInvocation with target nil.
++ (void)replaceForwardInvocation;
+
+- (void)fakeMethod;
+
+@end
 
 @implementation NSNull (IDPNil)
 
 + (void)load {
-//    IMP forwardInvocationIMP = [self instanceMethodForSelector:@selector(forwardInvocation:)];
     [self replaceMethodSignatureForSelector];
     [self replaceForwardInvocation];
 }
 
-// TODO: explain what it does
+@end
+
+@implementation NSNull (IDPNilPrivate)
+
 + (void)replaceMethodSignatureForSelector {
-    SEL methodSignatureForSelectorSEL = @selector(methodSignatureForSelector:);
+    SEL selector = @selector(methodSignatureForSelector:);
     
-    IDPBlockWithIMP block = ^(IMP implementation){
-        IDPMethodSignatureForSelectorIMP methodSignatureForSelectorIMP = (IDPMethodSignatureForSelectorIMP)implementation;
+    IDPBlockWithIMP block = ^(IMP implementation) {
+        IDPMethodSignatureForSelectorIMP methodIMP = (IDPMethodSignatureForSelectorIMP)implementation;
         
-        return (id)^(NSNull *null, SEL selector) {
-            id methodSignature = methodSignatureForSelectorIMP(null, methodSignatureForSelectorSEL, selector);
+        return (id)^(NSNull *nullObject, SEL selectorIMPParameter) {
+            NSMethodSignature *methodSignature = methodIMP(nullObject, selector, selectorIMPParameter);
             if (methodSignature) {
                 return methodSignature;
             }
             
-            return (id)methodSignatureForSelectorIMP(null, methodSignatureForSelectorSEL, @selector(fakeMethod));
+            methodSignature = methodIMP(nullObject, selector, @selector(fakeMethod));
+            methodSignature.nilForwarded = YES;
+            
+            return methodSignature;
         };
     };
     
-    [self setBlock:block forSelector:methodSignatureForSelectorSEL];
+    [self setBlock:block forSelector:selector];
 }
 
 + (void)replaceForwardInvocation {
+    SEL selector = @selector(forwardInvocation:);
     
+    IDPBlockWithIMP block = ^(IMP implementation) {
+        return (id)^(NSNull *nullObject, NSInvocation *invocation) {
+            IDPForwardInvocationIMP methodIMP = (IDPForwardInvocationIMP)implementation;
+            
+            if (invocation.methodSignature.nilForwarded) {
+                invocation.target = nil;
+                [invocation invoke];
+            } else {
+                methodIMP(nullObject, selector, invocation);
+            }
+        };
+    };
+    
+    [self setBlock:block forSelector:selector];
 }
 
 - (void)fakeMethod {
-
+    
 }
 
 @end
