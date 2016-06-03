f327b5762d9f71d4124ee4a32cec288aa4b2c231
Oleksa 'trimm' Korin <oleksandr.korin@idapgroup.com>
2016-06-03 12:19:28 +0300

    IDPObjectState -> initial commit

--- Module/FoundationKit/Shared/Classes/IDPObservableObject/IDPObjectState.h
+++ Module/FoundationKit/Shared/Classes/IDPObservableObject/IDPObjectState.h
@@ -8,4 +8,14 @@
 
 #import <Foundation/Foundation.h>
 
+#define __IDPObjectState(name, startName, start, ...) \
+    typedef enum { \
+        start, \
+        __VA_ARGS__, \
+        name##End, \
+        name##Count = name##End - startName - 1 \
+    } name
 
+#define IDPObjectState(name, ...) __IDPObjectState(name, name##Undefined, name##Undefined, __VA_ARGS__)
+
+#define IDPObjectStateChild(name, parent, ...) __IDPObjectState(name, name##Start, name##Start = parent##End - 1, __VA_ARGS__)
--- Module/FoundationKit/Shared/Classes/IDPObservableObject/IDPObjectState.m
+++ Module/FoundationKit/Shared/Classes/IDPObservableObject/IDPObjectState.m
@@ -8,4 +8,3 @@
 
 #import "IDPObjectState.h"
 
-
