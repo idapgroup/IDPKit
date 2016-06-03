bd929ba973a0cc173c9400bc73e5cd6221803a8b
Oleksa 'trimm' Korin <oleksandr.korin@idapgroup.com>
2016-06-03 12:55:27 +0300

    Cleaned up IDPModel
    IDPModel -> emptied
    IDPModelObserver, IDPModelProtocol -> removed

--- Module/FoundationKit/Shared/Classes/IDPModels/IDPModel.h
+++ Module/FoundationKit/Shared/Classes/IDPModels/IDPModel.h
@@ -6,18 +6,8 @@
 //  Copyright (c) 2013 IDAP Group. All rights reserved.
 //
 
-#import <Foundation/Foundation.h>
-
 #import "IDPObservableObject.h"
-#import "IDPModelProtocol.h"
 
-@interface IDPModel : IDPObservableObject <IDPModel>
-
-- (void)notifyObserversOfSuccessfulLoad;
-- (void)notifyObserversOfFailedLoad;
-- (void)notifyObserversOfCancelledLoad;
-- (void)notifyObserversOfChanges;
-- (void)notifyObserversOfChangesWithMessage:(NSDictionary *)message;
-- (void)notifyObserversOfUnload;
+@interface IDPModel : IDPObservableObject
 
 @end
