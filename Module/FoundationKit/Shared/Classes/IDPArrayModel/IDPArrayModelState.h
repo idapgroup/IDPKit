e37e931551e08d07e5e50b6ba36d6733a5e24ffe
Oleksa 'trimm' Korin <oleksandr.korin@idapgroup.com>
2016-06-22 15:51:34 +0300

    IDPArrayModelState -> initial commit

---
+++ Module/FoundationKit/Shared/Classes/IDPArrayModel/IDPArrayModelState.h
@@ -0,0 +1,12 @@
+//
+//  IDPArrayModelState.h
+//  iOS
+//
+//  Created by Oleksa 'trimm' Korin on 6/22/16.
+//  Copyright © 2016 IDAP Group. All rights reserved.
+//
+
+#import "IDPObjectState.h"
+
+IDPObjectState(IDPArrayModelState,
+               IDPArrayModelDidChange);
